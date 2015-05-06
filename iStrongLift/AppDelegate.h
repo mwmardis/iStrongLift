432a639a9be50b2693323bd08a856e4d00579c80
mwmardis <Michael Mardis>
2015-05-05 21:17:49 -0500

    Initial Commit

---
+++ iStrongLift/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iStrongLift
+//
+//  Created by Michael Mardis on 5/5/15.
+//  Copyright (c) 2015 CS378. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iStrongLift/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iStrongLift
+//
+//  Created by Michael Mardis on 5/5/15.
+//  Copyright (c) 2015 CS378. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
